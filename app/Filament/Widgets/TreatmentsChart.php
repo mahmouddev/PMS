f5b1820eff54249f675e95059b2019a2bc8f98f5
mahmouddev <m.abdelmoniem@kortoba.com>
2024-09-05 09:54:44 +0300

    first commit

---
+++ app/Filament/Widgets/TreatmentsChart.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Treatment;
+use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+use Illuminate\Contracts\Support\Htmlable;
+
+class TreatmentsChart extends ChartWidget
+{
+    protected static ?string $heading = 'Chart';
+
+    public function getHeading(): string | Htmlable | null
+    {
+        return  __('Chart');
+    }
+
+    protected function getData(): array
+    {
+        $data = Trend::model(Treatment::class)
+        ->between(
+            start: now()->subYear(),
+            end: now(),
+        )
+        ->perMonth()
+        ->count();
+ 
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Treatments',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(fn (TrendValue $value) => $value->date),
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'line';
+    }
+}
